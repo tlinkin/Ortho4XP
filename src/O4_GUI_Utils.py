d8fb6b9ec00fee1ad93ef1a3e05dc542ffafa968
shred86 <shred86@icloud.com>
2025-07-05 19:54:28 -0700

    fix mouse right-click for latest Python version

--- src/O4_GUI_Utils.py
+++ src/O4_GUI_Utils.py
@@ -1069,14 +1069,18 @@ class Ortho4XP_Custom_ZL(tk.Toplevel):
             0, 0, anchor=NW, image=self.photo
         )
         self.canvas.config(scrollregion=self.canvas.bbox(ALL))
-        if "dar" in sys.platform:
-            self.canvas.bind("<ButtonPress-2>", self.scroll_start)
-            self.canvas.bind("<B2-Motion>", self.scroll_move)
-            self.canvas.bind("<Control-ButtonPress-2>", self.delPol)
-        else:
-            self.canvas.bind("<ButtonPress-3>", self.scroll_start)
-            self.canvas.bind("<B3-Motion>", self.scroll_move)
-            self.canvas.bind("<Control-ButtonPress-3>", self.delPol)
+        # As of Python3.13.5, the following isn't needed for macOS
+        # if "dar" in sys.platform:
+        #     self.canvas.bind("<ButtonPress-2>", self.scroll_start)
+        #     self.canvas.bind("<B2-Motion>", self.scroll_move)
+        #     self.canvas.bind("<Control-ButtonPress-2>", self.delPol)
+        # else:
+        #     self.canvas.bind("<ButtonPress-3>", self.scroll_start)
+        #     self.canvas.bind("<B3-Motion>", self.scroll_move)
+        #     self.canvas.bind("<Control-ButtonPress-3>", self.delPol)
+        self.canvas.bind("<ButtonPress-3>", self.scroll_start)
+        self.canvas.bind("<B3-Motion>", self.scroll_move)
+        self.canvas.bind("<Control-ButtonPress-3>", self.delPol)
         self.canvas.bind(
             "<ButtonPress-1>", lambda event: self.canvas.focus_set()
         )
@@ -1541,12 +1545,15 @@ class Ortho4XP_Earth_Preview(tk.Toplevel):
         self.canvas.yview_moveto(y0 / self.resolution)
         self.nx0 = int((8 * x0) // self.resolution)
         self.ny0 = int((8 * y0) // self.resolution)
-        if "dar" in sys.platform:
-            self.canvas.bind("<ButtonPress-2>", self.scroll_start)
-            self.canvas.bind("<B2-Motion>", self.scroll_move)
-        else:
-            self.canvas.bind("<ButtonPress-3>", self.scroll_start)
-            self.canvas.bind("<B3-Motion>", self.scroll_move)
+        # As of Python3.13.5, the following isn't needed for macOS
+        # if "dar" in sys.platform:
+        #     self.canvas.bind("<ButtonPress-2>", self.scroll_start)
+        #     self.canvas.bind("<B2-Motion>", self.scroll_move)
+        # else:
+        #     self.canvas.bind("<ButtonPress-3>", self.scroll_start)
+        #     self.canvas.bind("<B3-Motion>", self.scroll_move)
+        self.canvas.bind("<ButtonPress-3>", self.scroll_start)
+        self.canvas.bind("<B3-Motion>", self.scroll_move)
         self.canvas.bind("<Double-Button-1>", self.select_tile)
         self.canvas.bind("<Shift-ButtonPress-1>", self.add_tile)
         self.canvas.bind("<Control-ButtonPress-1>", self.toggle_to_custom)
